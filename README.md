69bfd762a5a8465fc875258b6ba90201ffce5b26
hassanjabir <jabirhassan670@gmail.com>
2022-05-18 12:11:34 -0700

    lesson5.py

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# inspire-youth-in-stem
-# inspire-youth-in-stem
+###INSPIRE YOUTH IN STEM 
+## weeek one introduction to pyhon 
+day 1: installing python 
+
+Add a link to downloading python
+
+Add a link to downloading vs code
+
+[./images/lesson1.PNG]
+Strings
+   replace()
+    strip()
+    slice()
+    upper()
+    format()
+    lower()
+   
+
+
+## week two electronics for innovarors 
+
+## week three introduction to robotics 
+
+## weeek four java script ,css ,html
+
+## week five design thinking ,ux ,ui 
+
+## week six pich competition ,campus tour
+
--- lesson5.py
+++ lesson5.py
@@ -1,11 +1,57 @@
-#surface are of a cylinder
-from cmath import pi
+#methods
+
+# name = Ada Lovelace
+
+from cgitb import text
 
 
-radius = input("Enter the radius of a circle")
-pi = 3.142
-areaofacircle = pi * int(radius) * int(radius)
-print("the area of the circle is") + str(areaofacircle)
+name = "love lace"
+user_name= "lovelace"
+age  = 15
+#print + "i am "+ str(name)+ "and my age is" + str(age)
+#print f "i am "+ str (name)+ "and my age is " + str(age)
+#the format ()method
+#print ("my name is () and i am () years old".format(name,age) )
+#newline \n and tab \t
+#print(f"my name is (name) \n and i am (age) years old")
+print(user_name.lstrip())
 
-git config --global user.name "hassanjabir"
-git config --global user.email "jabirhassan670@gmail.com"
+city = "nairobi"
+print(city[2:])
+
+
+
+f_name = "yasmin abdi" # small letters
+# .upper() convert to upper case
+print(f_name.upper())
+
+s_name = "KINUTHUIA MBUGUA"
+# .lower() convert to lower case
+print(s_name.lower())
+
+#concatination =converting fromone data type to another
+# int > float float(x)
+# int -> float
+#int -> float 
+number = 6
+print(str(number))
+
+x =4 #x is an integer
+print(float(x))
+
+f_name = "jabir"
+
+s_name = "hassan"
+
+full_name = f_name + s_name
+print(full_name)
+name="Jay Hass"
+print(name.replace('J','6'))
+
+msg = "hello yas how are you mamaa"
+print(msg.split())
+
+print(len(msg))
+
+
+
