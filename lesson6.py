4527172181910919c9de179b0a83018f4a655e96
hassanjabir <jabirhassan670@gmail.com>
2022-05-19 11:15:26 +0300

    lesson6.py

---
+++ lesson6.py
@@ -0,0 +1,28 @@
+#owner  = "mojo jojo"
+#plate_number = ["H1234","Y1234","S1234"]
+#motorcycle = [ "honda","yamaha","suzuki"]
+#print(motorcycle)
+#accesing items using index
+#print(motorcycle)
+#print(motorcycle[1])
+#motorcycle[0]= "buggati"#changing element in a list
+#print ( motorcycle)
+#adding an element in a list
+#motorcycle.append("Buggati yonaha")
+#print(motorcycle)
+#removing an item from a list
+#task"""print the plate numbers
+#print(motorcycle[0],str (plate_number))
+#print(str (motorcycle[0]) , str(plate_number[0]) , str(motorcycle[1]) , str(plate_number[1]) , str(motorcycle[2]) , str(plate_number[2]))
+#deleting an item from a list use del
+#del motorcycle[0]
+#print(motorcycle)
+#popped_motorcycle = motorcycle.pop()
+#print(popped_motorcycle)
+#my name is mojo jojo and i own a motorcycle plate number
+#print("my name is " + str(owner) + " and i own a" , str(motorcycle[0]) , str(plate_number[0]))
+#print(f"my name is {owner}" "and i own a {motorcycle[0]} {plate_number[0]}")
+#removing an item from a list
+#motorcycle.remove("honda")
+#print(motorcycle)
+
---
+++ loops.py
@@ -0,0 +1,9 @@
+school=["joy","hope","mercy","happy"]
+pupil=["peter","mark","james","brian",]
+print(school[0],(pupil[0]))
+print(school[1]),(pupil[1])
+print(school[2]),(pupil[2])
+print(school[3]),(pupil[3])
+sentence= "hello i am   (pupil)"
+for pupil in pupil:
+    print(f'hello i am pupil {pupil}')
